0da748486b2c12112081bcfded97377a3b66902c
jichuangsi <school@jichuangsi>
2019-11-03 10:13:54 +0800

    [jiewne.wu]fix bug

--- database/install_sql/20191031.sql
+++ database/install_sql/20191031.sql
@@ -1,3 +1,3 @@
-INSERT INTO `kppw_config` (`alias`, `rule`, `type`, `title`, `desc`) VALUES ('wx_appid', 'wxc91dff3d90d14c13', 'weixin', '微信appid', '微信appid'), ('wx_secret', 'be8cfbba1effe57abb0b08ce6d3e0834', 'weixin', '微信secret', '微信secret');
+INSERT INTO `kppw_config` (`alias`, `rule`, `type`, `title`, `desc`) VALUES ('wx_appid', 'wxc91dff3d90d14c13', 'weixin', '微信appid', '微信appid'), ('wx_secret', '73ccda962d59c3d9c14c09f13019522d', 'weixin', '微信secret', '微信secret');
 
 alter table `kppw_user_detail` modify column `wechat` varchar(100);
